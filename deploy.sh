854c3170bcd6ff78bbaba2519e01e4cb5ef3c8bb
dangen-effy <dangen.effy@gmail.com>
2020-02-23 14:39:08 +0900

    rebuilding site Sun Feb 23 14:39:08 KST 2020

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,9 @@ hugo -t hugo-theme-yinyang
 
 # Go To Public folder
 cd public
+
+echo -e "\033[0;32mNow in public folder\033[0m"
+
 # Add changes to git.
 git add .
 
@@ -23,6 +26,7 @@ git push origin master
 # Come Back up to the Project Root
 cd ..
 
+echo -e "\033[0;32mNow in blog folder\033[0m"
 
 # blog 저장소 Commit & Push
 git add .
